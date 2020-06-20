c78e0cba8dd87f6498f2297aa2a1035d3df00228
AislinnRay <aislinn.ray.m@gmail.com>
2020-06-19 22:54:50 -0600

    commented out unused portions of redux

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -11,31 +11,31 @@ const initialState = {
   }
   }
 
-  const LOGIN_USER = 'LOGIN_USER'
-  const LOGOUT_USER = 'LOGOUT_USER'
-  const GET_USER = 'GET_USER'
-  const FULFILLED = "_FULFILLED"
+  // const LOGIN_USER = 'LOGIN_USER'
+  // const LOGOUT_USER = 'LOGOUT_USER'
+  // const GET_USER = 'GET_USER'
+  // const FULFILLED = "_FULFILLED"
   const SET_USER = 'SET_USER'
 
-export function loginUser(user){
-  return {
-      type: LOGIN_USER,
-      payload: user
-    }
-}
+// export function loginUser(user){
+//   return {
+//       type: LOGIN_USER,
+//       payload: user
+//     }
+// }
 
-export function logoutUser(user){
-    return {
-        type: LOGOUT_USER, payload: user
-    }
-}
+// export function logoutUser(user){
+//     return {
+//         type: LOGOUT_USER, payload: user
+//     }
+// }
 
-export function getUser(){
-    const user = axios.get('/auth/user')
-    return {
-        type: GET_USER, payload: initialState
-    }
-}
+// export function getUser(){
+//     const user = axios.get('/auth/user')
+//     return {
+//         type: GET_USER, payload: initialState
+//     }
+// }
 
 export function setUser(payload){
     return {
@@ -45,16 +45,16 @@ export function setUser(payload){
   
   export default function (state = initialState, action) {
     switch (action.type) {
-      case LOGIN_USER:
-        return { ...state, user: action.payload };
-      case LOGOUT_USER:
-        return initialState;
-      case GET_USER + '_PENDING':
-          return state
-      case GET_USER + FULFILLED:
-          return {...state, user: action.payload}
-      case GET_USER + '_REJECTED':
-          return initialState
+      // case LOGIN_USER:
+      //   return { ...state, user: action.payload };
+      // case LOGOUT_USER:
+      //   return initialState;
+      // case GET_USER + '_PENDING':
+      //     return state
+      // case GET_USER + FULFILLED:
+      //     return {...state, user: action.payload}
+      // case GET_USER + '_REJECTED':
+      //     return initialState
     case SET_USER:
         return {...state, user: action.payload}  
       default:

4582ddc57c280bdfeed132995d1401e64e41207a
AislinnRay <aislinn.ray.m@gmail.com>
2020-07-03 19:18:04 -0600

    hosting stuff

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const contactUtil = require('./utils/contactEmailUtil')
 
 //Hosting
 //app.use(express.static(__dirname + '/../build'));
-app.use(express.static(${__dirname}/../build));
+app.use(express.static(`${__dirname}/../build`));
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build/index.html'))
 // });
